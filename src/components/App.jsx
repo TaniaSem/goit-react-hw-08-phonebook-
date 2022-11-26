cbf09e2ee54bca1be9493a615bbeb56382dd639c
TaniaSem <semenchuktetiana@gmail.com>
2022-11-26 22:22:09 +0200

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
+export const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route>
+          <Route />
+          <Route />
+          <Route />
+          <Route />
+        </Route>
+      </Routes> 
+    </div>
+  );
+};
